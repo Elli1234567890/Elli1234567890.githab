d91f8f0c5be29d8cef58f77f0db5a157e5fb89f7
Trubienko <73603076+Elli1234567890@users.noreply.github.com>
2023-10-09 14:25:15 +0300

    Update script.js

--- Задание 5/script.js
+++ Задание 5/script.js
@@ -1,1 +1,42 @@
-s
+<h3>Расчет стоимости продуктов</h3>
+<select id="cvet">
+<option value="serii">Серый</option>
+<option value="geltii">Желтый</option>
+<option value="goluboi">Голубой</option>
+</select>
+<input type="text" id="shirina" value="" placeholder="укажите ширину помещения">
+<input type="text" id="dlina" value="" placeholder="укажите длину помещения">
+<button onclick="raschitat();">Рассчитать стоимость</button>
+<div id="ploschad"></div>
+<div id="stoimost"></div>
+<script type="text/jаvascript">
+function raschitat() {
+cvet  = document.getElementById('cvet').value;
+switch (cvet) {
+   case "serii":
+      cena = 440;
+      break
+   case "goluboi":
+      cena = 480;
+      break   
+    case "geltii":
+      cena = 380;
+      break   
+   default:
+      cena = 440;
+      break
+}
+shirina  = document.getElementById('shirina').value;
+dlina  = document.getElementById('dlina').value;
+if(shirina == ""){
+alert("Вы не указали ширину");
+} else if(dlina == ""){
+alert("Вы не указали длину");
+} else {
+ploschad = parseFloat (shirina)* parseFloat (dlina);
+document.getElementById('ploschad').innerHTML = "Площадь равна: "+ ploschad +" кв. м.";
+stoimost = ploschad*cena;
+document.getElementById('stoimost').innerHTML = "Стоимость равна: "+ stoimost +" р.";
+}
+}
+</script>
