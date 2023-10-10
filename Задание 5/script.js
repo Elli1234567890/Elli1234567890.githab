c5887270f677f5c4ba746caa943b38a0c59485ac
Trubienko <73603076+Elli1234567890@users.noreply.github.com>
2023-10-11 00:13:32 +0300

    Update script.js

--- Задание 5/script.js
+++ Задание 5/script.js
@@ -1,36 +1,27 @@
 <h3>Расчет стоимости продуктов</h3>
-<select id="prodact">
-<option value="candy">Конфеты</option>
-<option value="marmelad">Мармелад</option>
-<option value="cherry">Черешня</option>
-<option value="banana">Бананы</option>
-</select>
-<input type="text" id="ves" value="" placeholder="укажите вес продуктов">
-<button onclick="raschitat();">Рассчитать стоимость</button>
-<div id="stoimost"></div>
-<script type="text/jаvascript">
+
 function raschitat() {
-cvet  = document.getElementById('prodact').value;
-switch (prodact) {
-   case "candy":
-      cena = 20;
-      break
-   case "marmelad":
-      cena = 100;
-      break   
-    case "cherry":
-      cena = 50;
-      break 
-    case "banana":
-      cena = 70;
-      break 
+   cvet  = document.getElementById('prodact').value;
+   switch (prodact) {
+      case "candy":
+         cena = 20;
+         break;
+      case "marmelad":
+         cena = 100;
+         break;  
+       case "cherry":
+         cena = 50;
+         break; 
+       case "banana":
+         cena = 70;
+         break;
+   }
+   ves  = document.getElementById('ves').value;
+   if(ves == ""){
+      alert("Вы не указали вес продуктов");
+   }
+   else {
+      stoimost = ves*cena/100;
+      document.getElementById('stoimost').innerHTML = "Стоимость равна: "+ stoimost +" р.";
+   }
 }
-ves  = document.getElementById('ves').value;
-if(ves == ""){
-alert("Вы не указали вес продуктов");
-} else {
-stoimost = ves*cena;
-document.getElementById('stoimost').innerHTML = "Стоимость равна: "+ stoimost +" р.";
-}
-}
-</script>
