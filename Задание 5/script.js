0e4135dd130139b971eff5e2bb8348d0b48df975
Trubienko <73603076+Elli1234567890@users.noreply.github.com>
2023-10-11 23:01:02 +0300

    Update script.js

--- Задание 5/script.js
+++ Задание 5/script.js
@@ -1,13 +1,16 @@
-document.getElementById('button').onclick = function(){
+<!-- document.getElementById('button').onclick = function(){
     st();
 };
+-->
 
-
-function st() {
+let = st() => {
   let v = document.getElementsByName("ves");
   let c = document.getElementsByName("cena");
   let stoim = v * c[0].value / 100;
-  <!-- console.log(st[0].value); -->
+  if (v <= 0 || !Number(v))alert("Так сложно ввести положительное число > 0?");
+  else return "Стоимость: " + String(c[0].value * v / 100) + "руб за 100г";
+  
+  <!-- console.log(st[0].value);
   
   document.getElementById('stoimost').innerHTML = stoim;
   
@@ -15,10 +18,11 @@ function st() {
     alert("Так сложно ввести положительное число > 0?");
   }
   return false;
-}
+} -->
 
-window.addEventListener('DOMContentLoaded, (event){
+window.addEventListener('DOMContentLoaded, (event) => {
     console.log("DOM fully loaded and parsed");
-    let button = document.getElementById("button");
-    button.addEventListener("Так сложно ввести положительные числа?", st());
+    let button = document.getElementById("form");
+    button.addEventListener("click", st());
 });
+
