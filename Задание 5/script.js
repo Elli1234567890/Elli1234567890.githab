9ce360bd435524d3b05fd27f0101fdbfa0bc61ac
Trubienko <73603076+Elli1234567890@users.noreply.github.com>
2023-10-11 23:27:23 +0300

    Update script.js

--- Задание 5/script.js
+++ Задание 5/script.js
@@ -1,28 +1,32 @@
-<!-- document.getElementById('button').onclick = function(){
-    st();
-};
--->
+// document.getElementById('button').onclick = function(){
+//    st();
+//};
 
-let = st() => {
+
+let st = () => {
   let v = document.getElementsByName("ves");
   let c = document.getElementsByName("cena");
   let stoim = v * c[0].value / 100;
   if (v <= 0 || !Number(v))alert("Так сложно ввести положительное число > 0?");
   else return "Стоимость: " + String(c[0].value * v / 100) + "руб за 100г";
+};
+//  <!-- console.log(st[0].value);
   
-  <!-- console.log(st[0].value);
+//  document.getElementById('stoimost').innerHTML = stoim;
   
-  document.getElementById('stoimost').innerHTML = stoim;
-  
-  if (v <= 0 || !Number(v)) {
-    alert("Так сложно ввести положительное число > 0?");
-  }
-  return false;
-} -->
+//  if (v <= 0 || !Number(v)) {
+//    alert("Так сложно ввести положительное число > 0?");
+//  }
+//  return false;
+//} -->
 
+
+    
 window.addEventListener('DOMContentLoaded, (event) => {
     console.log("DOM fully loaded and parsed");
     let button = document.getElementById("form");
     button.addEventListener("click", st());
 });
+
+
 
