04059a6fe256b74c28691dab4e2a191eb380816b
Trubienko <73603076+Elli1234567890@users.noreply.github.com>
2023-10-11 19:45:05 +0300

    Update script.js

--- Задание 5/script.js
+++ Задание 5/script.js
@@ -1,14 +1,13 @@
-button.onclick = function() {
+function st() {
   let v = document.getElementsByName("ves");
   let c = document.getElementsByName("cena");
-  let stoim = document.getElementById("stoimost");
-}
-   if (v[0].value < 0 || c[0].value < 0 || !Number(v[0].value) || !Number(c[0].value)) {
-    alert("Так сложно ввессти положительные числа?");
-    r.innerHtML = none;
+  let stoim = document.getElementsById("stoimost");
+  stoim = v[0].value * c[0].value / 100;
+  let st = document.getElementsByName("stt");
+  console.log(st[0].value);
+  
+  if (v[0].value < 0 || c[0].value < 0 || !Number(v[0].value) || !Number(c[0].value)) {
+    alert("Так сложно ввести положительные числа?");
   }
-stoim = v[0].value * c[0].value / 100;
-document.getElementById("button").innerHTML = stoim; 
+  return false;
 }
-
-
