14cc8ad93ab631247fb39c466c01fb6343bbf927
Trubienko <73603076+Elli1234567890@users.noreply.github.com>
2023-10-16 14:52:51 +0300

    Update script.js

--- Задание 5/script.js
+++ Задание 5/script.js
@@ -8,7 +8,7 @@ function st(event) {
   if (v <= 0 || !Number(v))
     alert("Так сложно ввести положительное число?");
   else
-    text.innerText = "Стоимость: " + String(c[0].value * v / 100) + "руб за 100г";
+    text.innerText = "Стоимость: " + String(c.value * v / 100) + "руб за 100г";
 }
 
 window.addEventListener('DOMContentLoaded, (event) => {
