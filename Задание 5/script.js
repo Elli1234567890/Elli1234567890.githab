c5adf52e50095eac6bc2839c864bf10e9a0d980a
Trubienko <73603076+Elli1234567890@users.noreply.github.com>
2023-10-16 15:01:19 +0300

    Update script.js

--- Задание 5/script.js
+++ Задание 5/script.js
@@ -3,19 +3,17 @@ let text = document.getElementById('stoimost')
 function st(event) {
   event.preventDefault();
   let v = document.getElementById("ves");
+  let count = v.value;
   let c = document.getElementById("cena");
-  let stoim = v.value * c.value / 100;
-  if (v.value <= 0 || !Number(v.value))
+  let stoim = count * c.value / 100;
+  if (count <= 0 || !Number(count))
     alert("Так сложно ввести положительное число?");
   else
-    text.innerText = "Стоимость: " + String(c.value * v / 100) + "руб за 100г";
+    text.innerText = "Стоимость: " + String(c.value * count / 100) + "руб за 100г";
 }
 
-window.addEventListener('DOMContentLoaded, (event) => {
+window.addEventListener('DOMContentLoaded', (event) => {
      console.log("DOM fully loaded and parsed");
      let button = document.getElementById("form");
      button.addEventListener("click", st);
 });
-
-
-
