474d1ac1375eb77d3a3a6b30f1d64c4d5e6c4c37
Trubienko <73603076+Elli1234567890@users.noreply.github.com>
2023-10-16 14:53:52 +0300

    Update script.js

--- Задание 5/script.js
+++ Задание 5/script.js
@@ -5,7 +5,7 @@ function st(event) {
   let v = document.getElementById("ves");
   let c = document.getElementById("cena");
   let stoim = v.value * c.value / 100;
-  if (v <= 0 || !Number(v))
+  if (v.value <= 0 || !Number(v))
     alert("Так сложно ввести положительное число?");
   else
     text.innerText = "Стоимость: " + String(c.value * v / 100) + "руб за 100г";
@@ -14,7 +14,7 @@ function st(event) {
 window.addEventListener('DOMContentLoaded, (event) => {
      console.log("DOM fully loaded and parsed");
      let button = document.getElementById("form");
-     button.addEventListener("click", st(self));
+     button.addEventListener("click", st);
 });
 
 
