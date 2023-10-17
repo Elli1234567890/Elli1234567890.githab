b42be5598a30c8c83feafebe5a2c31c3397bc4d7
Trubienko <73603076+Elli1234567890@users.noreply.github.com>
2023-10-17 20:43:38 +0300

    Update script.js

--- Задание 6/script.js
+++ Задание 6/script.js
@@ -1,1 +1,19 @@
+let text = document.getElementById('stoimost')
 
+function st(event) {
+  event.preventDefault();
+  let v = document.getElementById("ves");
+  let count = v.value;
+  let c = document.getElementById("cena");
+  let stoim = count * c.value / 100;
+  if (count <= 0 || !Number(count))
+    alert("Так сложно ввести положительное число?");
+  else
+    text.innerText = "Стоимость: " + String(c.value * count / 100) + "руб за 100г";
+}
+
+window.addEventListener('DOMContentLoaded', (event) => {
+     console.log("DOM fully loaded and parsed");
+     let button = document.getElementById("form");
+     button.addEventListener("click", st);
+});
