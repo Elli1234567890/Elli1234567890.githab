58ad07573c154e69b18662723375fcaf424ee63c
Trubienko <73603076+Elli1234567890@users.noreply.github.com>
2023-10-18 20:03:50 +0300

    Update script.js

--- Задание 6/script.js
+++ Задание 6/script.js
@@ -1,59 +1,3 @@
-// let text = document.getElementById('stoimost')
-// let v = document.getElementById("ves");
-// let c = document.getElementById("cena");
-// let radio = document.getElementsByName("radio");
-// let rad = document.getElementById("rad");
-// let ch = document.getElementsByName("check");
-// let checks = document.getElementById("checks")
-// rad.style.display = "none";
-// checks.style.display = "none";
-
-
-
-// function st(event) {
-//   event.preventDefault();
-
-//   rad.style.display= (c.value === "40"? "block" : "none");
-//   checks.style.display = (c.value === "60"? "block" : "none");
-
-//   if (v.value <= 0 || !Number(v.value))
-//   {
-//     alert("Так сложно ввести положительное число?");
-//     console.log(v.value);
-//   }
-    
-//   else{
-//     console.log("Работает");
-//     let radioCost = 0
-//     for (let i = 0; i < radio.length;i++)
-//     {
-//       if (radio[i].checked)
-//         radioCost += Number(radio[i].value);
-//     }
-
-//     let checkCost = 0
-//     for (let i = 0; i < ch.length;i++)
-//     {
-//       if (ch[i].checked)
-//         checkCost += Number(ch[i].value);
-//     }
-
-//     console.log(v.value, v.value, radioCost, checkCost);
-//     text.innerText = "Стоимость: " + String((Number(c.value)  + ((rad.style.display == "block")? radioCost : 0) + ((checks.style.display == "block")? checkCost : 0) )* v.value);
-//   }
-  
-// };
-
-
-// window.addEventListener('DOMContentLoaded', (event) => {
-//     console.log("DOM fully loaded and parsed");
-  
-//     c.addEventListener("click", st);
-//     v.addEventListener("change",st);
-//     rad.addEventListener("change",st);
-//     checks.addEventListener("change",st);
-// });С‡РјРѕРє)
-
 let text = document.getElementById('stoimost')
 let v = document.getElementById("ves");
 let c = document.getElementById("cena");
@@ -64,8 +8,6 @@ let checks = document.getElementById("checks")
 rad.style.display = "none";
 checks.style.display = "none";
 
-
-
 function st(event) {
   event.preventDefault();
 
@@ -74,12 +16,12 @@ function st(event) {
 
   if (v.value <= 0 || !Number(v.value))
   {
-    alert("РўР°Рє СЃР»РѕР¶РЅРѕ РІРІРµСЃС‚Рё РїРѕР»РѕР¶РёС‚РµР»СЊРЅРѕРµ С‡РёСЃР»Рѕ?");
+    alert("Так сложно ввести положительное число?");
     console.log(v.value);
   }
     
   else{
-    console.log("Р’СЃС‘ РЅРѕСЂРј");
+    console.log("Работает");
     let radioCost = 0
     for (let i = 0; i < radio.length;i++)
     {
@@ -95,16 +37,14 @@ function st(event) {
     }
 
     console.log(v.value, v.value, radioCost, checkCost);
-    text.innerText = "РЎС‚РѕРёРјРѕСЃС‚СЊ: " + String((Number(c.value)  + ((rad.style.display == "block")? radioCost : 0) + ((checks.style.display == "block")? checkCost : 0) )* v.value);
+    text.innerText = "Стоимость: " + String((Number(c.value)  + ((rad.style.display == "block")? radioCost : 0) + ((checks.style.display == "block")? checkCost : 0) )* v.value);
   }
-
-
+  
 };
 
-
 window.addEventListener('DOMContentLoaded', (event) => {
-     console.log("DOM fully loaded and parsed");
-
+    console.log("DOM fully loaded and parsed");
+  
     c.addEventListener("click", st);
     v.addEventListener("change",st);
     rad.addEventListener("change",st);
